66bafe95bd04b1e19dc0c6410af070b67815b801
vsaraminev <v.saraminev@abv.bg>
2017-06-02 22:43:06 +0300

    Add Third Task

---
+++ 01.CIntroAndBasicSyntax/EmployeeData/EmployeeData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EmployeeData
+{
+    public class EmployeeData
+    {
+        public static void Main()
+        {
+            var name = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+            var ID = int.Parse(Console.ReadLine());
+            var salary = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Employee ID: {ID:d8}");
+            Console.WriteLine($"Salary: {salary:f2}");
+        }
+    }
+}
