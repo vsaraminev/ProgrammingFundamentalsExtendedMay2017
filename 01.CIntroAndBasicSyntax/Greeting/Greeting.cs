2a5bf6e33364e223f1b0d6c056dc2614cf465e2b
vsaraminev <v.saraminev@abv.bg>
2017-06-02 22:32:30 +0300

    Add first task

---
+++ 01.CIntroAndBasicSyntax/Greeting/Greeting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Greeting
+{
+    public class Greeting
+    {
+        public static void Main()
+        {
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
