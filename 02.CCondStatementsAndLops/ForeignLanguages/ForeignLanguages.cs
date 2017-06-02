855e669b4184ce2507b4f80e53adcc1621abb142
vsaraminev <v.saraminev@abv.bg>
2017-06-02 22:50:04 +0300

    Add Cond. Statements And Loops Lab

---
+++ 02.CCondStatementsAndLops/ForeignLanguages/ForeignLanguages.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ForeignLanguages
+{
+    public class ForeignLanguages
+    {
+        public static void Main()
+        {
+            var country = Console.ReadLine();
+
+            if (country == "England" || country == "USA")
+            {
+                Console.WriteLine("English");
+            }
+            else if (country == "Spain" || country == "Argentina" || country =="Mexico")
+            {
+                Console.WriteLine("Spanish");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
---
+++ 02.CCondStatementsAndLops/MultiTable/MultiTable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MultiTable
+{
+    public class MultiTable
+    {
+        public static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());            
+            
+            for (int i = 1; i <= 10; i++)
+            {
+                var result = number * i;
+                Console.WriteLine($"{number} X {i} = {result}");
+            }
+        }
+    }
+}
