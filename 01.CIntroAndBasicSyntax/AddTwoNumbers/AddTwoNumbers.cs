82ded9f88c97b12e3bc7297f5a02e78a4103a220
vsaraminev <v.saraminev@abv.bg>
2017-06-02 22:40:22 +0300

    Add Second Task

---
+++ 01.CIntroAndBasicSyntax/AddTwoNumbers/AddTwoNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AddTwoNumbers
+{
+    public class AddTwoNumbers
+    {
+        public static void Main()
+        {
+            var firstNum = int.Parse(Console.ReadLine());
+            var secondNum = int.Parse(Console.ReadLine());
+            var result = firstNum + secondNum;
+
+            Console.WriteLine($"{firstNum} + {secondNum} = {result} ");
+        }
+    }
+}
