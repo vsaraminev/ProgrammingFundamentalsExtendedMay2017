790affc46cde498dea5cd153b360f1f3f4967ba7
vsaraminev <v.saraminev@abv.bg>
2017-09-04 22:03:59 +0300

    Add Task 18

---
+++ 18.Dictionaries/CountRealNumbers/CountRealNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CountRealNumbers
+{
+    public class CountRealNumbers
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToList();
+
+            var dictionary = new SortedDictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (!dictionary.ContainsKey(number))
+                {
+                    dictionary[number] = 0;
+                }
+
+                dictionary[number]++;
+            }
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
---
+++ 18.Dictionaries/OddOccurrences/OddOccurrences.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OddOccurrences
+{
+    public class OddOccurrences
+    {
+        public static void Main()
+        {
+            var words = Console.ReadLine()
+                .ToLower()
+                .Split(' ');
+
+            var dictionary = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (!dictionary.ContainsKey(word))
+                {
+                    dictionary[word] = 0;
+                }
+
+                dictionary[word]++;
+            }
+
+            var result = new List<string>();
+
+            foreach (var item in dictionary)
+            {
+                if (item.Value % 2 != 0)
+                {
+                    result.Add(item.Key);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ",result));
+        }
+    }
+}
